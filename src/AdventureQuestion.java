6f8824e87896a0bd3214be634f3eaf424ddca790
BenAgin1 <benagin1@gmail.com>
2018-03-09 11:56:13 -0500

    Added AdventureQuestion class, and integrated it into project. working on formatting.

---
+++ src/AdventureQuestion.java
@@ -0,0 +1,47 @@
+public class AdventureQuestion
+{
+    private String question;
+    private String answerA,answerB,answerC;
+
+    public AdventureQuestion(String question, String answerA, String answerB, String answerC)
+    {
+        this.question=question;
+        this.answerA=answerA;
+        this.answerB=answerB;
+        this.answerC=answerC;
+    }
+
+    public  String getQuestion()
+    {
+        return question;
+    }
+
+    public  String getAnswerA()
+    {
+        return answerA.substring(6); //because the first 2 chars determine the effect on sanity and Iago trust.
+    }
+
+    public  String getAnswerB()
+    {
+        return answerB.substring(6); //because the first 2 chars determine the effect on sanity and Iago trust.
+    }
+
+    public String getAnswerC()
+    {
+        return answerC.substring(6); //because the first 2 chars determine the effect on sanity and Iago trust.
+    }
+    public  int getEffects(int whichBar, String answer) //0, 2, 4 for each bar
+    {
+        return Integer.parseInt(answer.substring(whichBar,whichBar+2));
+    }
+
+
+
+
+    //public static int getBarMods
+
+
+
+}
+
+//250000Avoid the question.
--- src/Screen.java
+++ src/Screen.java
@@ -12,8 +12,23 @@ public class Screen extends JFrame implements ActionListener
     public final int MAX_NUMBER_ROWS= 5;
     public final int MAX_NUMBER_COLS=30;
 
+    AdventureQuestion[] questions= {new AdventureQuestion("Whats up?", "123242nothing much", "674839Hillll", "984523HUihefsef")};
+
     // Declare an array of "fortunes" (strings):
     String[] fortunes = {"You will be happy for today only. eifjiesjfoisejfiejijsieofjiosej foisejfoisejfoisjefoiseo ifjisoejf", "You find Desdemona sleeping in bed, what do you do?", "You will find happiness.", "You will find peace."};
+    public int questionPlace=0;
+
+    AdventureQuestion questionNow = questions[questionPlace];
+
+    JButton buttonA= new JButton("A");
+    JButton buttonB= new JButton("B");
+    JButton buttonC= new JButton("C");
+
+    JProgressBar Respect= new JProgressBar(0,100);
+    JProgressBar Sanity= new JProgressBar(0,100);
+    JProgressBar TrustInIago= new JProgressBar(0,100);
+
+    //String[] question1= {"" , "", "" , ""};
 
     private JTextArea displayQ;
     private JTextArea displayA;
@@ -33,7 +48,7 @@ public class Screen extends JFrame implements ActionListener
 
 
 
-        JButton buttonA = new JButton("A");
+        //JButton buttonA = new JButton("A");
         buttonA.addActionListener(this);
         //buttonA.setBounds(100,400,500,20);
         displayA = new JTextArea(MAX_NUMBER_ROWS,MAX_NUMBER_COLS);
@@ -44,7 +59,7 @@ public class Screen extends JFrame implements ActionListener
         displayA.setEditable(false);
 
 
-        JButton buttonB = new JButton("B");
+        //JButton buttonB = new JButton("B");
         buttonB.addActionListener(this);
 
         displayB = new JTextArea(MAX_NUMBER_ROWS,MAX_NUMBER_COLS);
@@ -54,7 +69,7 @@ public class Screen extends JFrame implements ActionListener
         displayB.setBackground(Color.WHITE);
         displayB.setEditable(false);
 
-        JButton buttonC = new JButton("C");
+        //JButton buttonC = new JButton("C");
         buttonC.addActionListener(this);
 
         displayC = new JTextArea(MAX_NUMBER_ROWS,MAX_NUMBER_COLS);
@@ -63,6 +78,7 @@ public class Screen extends JFrame implements ActionListener
         displayC.setText("Kill yourself.");
         displayC.setBackground(Color.WHITE);
         displayC.setEditable(false);
+
 
 
 
@@ -81,26 +97,88 @@ public class Screen extends JFrame implements ActionListener
         c.add(displayC);
 
 
+        c.add(Respect);
+        c.add(Sanity);
+        c.add(TrustInIago);
 
+        Respect.setString("Respect");
+        Respect.setStringPainted(true);
+        Respect.setValue(80);
+
+        Sanity.setString("Sanity");
+        Sanity.setStringPainted(true);
+        Sanity.setValue(100);
+
+        TrustInIago.setString("TrustInIago");
+        TrustInIago.setStringPainted(true);
+        TrustInIago.setValue(50);
     }
 
     public void actionPerformed(ActionEvent e)
     {
+        if (e.getSource() == buttonA)
+        {
+            adjustBar('a');
+        }
+        if (e.getSource() == buttonB)
+        {
+            adjustBar('b');
+        }
+        if (e.getSource()== buttonC)
+        {
+            adjustBar('c');
+        }
+
         // Pick and display a random fortune:
-        String fortune = fortunes[(int) (fortunes.length * Math.random())];
-        displayQ.setText("  " + fortune);
+
+        //String fortune = fortunes[questionPlace];
+
+
+        questionPlace++;
+        if (questionPlace ==2 && TrustInIago.getValue() <= 40)
+            questionPlace++;
+
+        //questionNow
+
+        displayQ.setText("  " + questions[questionPlace].getQuestion());
+        displayB.setText(questions[questionPlace].getAnswerB());
         ding.play();
 
         //
-
     }
+
+    public void adjustBar(char ans)
+    {
+        if (ans=='a')
+        {
+            Respect.setValue( Respect.getValue()+ questionNow.getEffects(0, questionNow.getAnswerA()));
+            Sanity.setValue( Sanity.getValue()+ questionNow.getEffects(2, questionNow.getAnswerA()));
+            TrustInIago.setValue( TrustInIago.getValue()+ questionNow.getEffects(4, questionNow.getAnswerA()));
+
+        }
+        if (ans=='b')
+        {
+            Respect.setValue( Respect.getValue()+ questionNow.getEffects(0, questionNow.getAnswerB()));
+            Sanity.setValue( Sanity.getValue()+ questionNow.getEffects(2, questionNow.getAnswerB()));
+            TrustInIago.setValue( TrustInIago.getValue()+ questionNow.getEffects(4, questionNow.getAnswerB()));
+
+        }
+        if (ans=='c')
+        {
+            Respect.setValue( Respect.getValue()+ questionNow.getEffects(0, questionNow.getAnswerC()));
+            Sanity.setValue( Sanity.getValue()+ questionNow.getEffects(2, questionNow.getAnswerC()));
+            TrustInIago.setValue( TrustInIago.getValue()+ questionNow.getEffects(4, questionNow.getAnswerC()));
+
+        }
+    }
+
 
     public static void main(String[] args)
     {
         JFrame window = new Screen();
-        window.setBounds(100, 100, 385, 500);
+        window.setBounds(100, 100, 385, 525);
         window.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        window.setResizable(true);
+        window.setResizable(false);
         window.setVisible(true);
     }
 }
