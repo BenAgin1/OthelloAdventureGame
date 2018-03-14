1d3a597d267905b6f004cd65de1e0cb48154dea9
BenAgin1 <benagin1@gmail.com>
2018-03-13 22:33:17 -0400

    Added tons of things: changed end game format to close on button press. Changed number percentages to be realistic. skips over the drinking thing to work with story line.
    and just cleanup of code.

--- src/Screen.java
+++ src/Screen.java
@@ -1,8 +1,6 @@
 import com.sun.org.apache.regexp.internal.RE;
 
-import java.awt.Color;
-import java.awt.Container;
-import java.awt.FlowLayout;
+import java.awt.*;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import java.awt.image.BufferedImage;
@@ -14,45 +12,27 @@ import javax.swing.*;
 public class Screen extends JFrame implements ActionListener
 {
     private static final EasySound ding = new EasySound("ding.wav");
-    //private static final EasySound damn = new EasySound("ding.wav");
 
-
-
-
-
-    public final int MAX_NUMBER_ROWS= 5;
-    public final int MAX_NUMBER_COLS=30;
+    private final int MAX_NUMBER_ROWS= 5;
+    private final int MAX_NUMBER_COLS=30;
 
     AdventureQuestion[] questions= //add the questions here, and effects as well
     {//I will edit it later to not show the numbers right in front.
             //NEW: above 50 is increase, below is decrease.
             new AdventureQuestion("THIS IS A FILL, WONT SHOW", "505050Avoid the question", "505050Hillll", "505050HUihefsef"),
-            new AdventureQuestion("Barbantio accuses you of bewitching his daughter into marrying you while the entire Venentian senate is watching. How do you respond?", "005050Avoid the question", "990000Politely deny it", "506050Attack Barbantio"),
-
-
-
-
-
-
-            new AdventureQuestion( "You made it safely to Cyprus", "205050Throw a party to celebrate!","705050Get back to work, no time for playing.", " "),
-            new AdventureQuestion("Desdemona repeatedly asks you to forgive Cassio and make him lieutenant again. How do you respond to her?","505030Forgive Cassio.","504050Put it off.","504040Ignore her."), //only if answer before was throw a party
-
-            new AdventureQuestion("Lodovico greets you and Desdemona with news from Venice. Cassio will replace you, and you must return back to Venice. What do you do?","004050Hit Desdemona.","204050Call Desdemona a Strumpet.","606020Choose not to be mad at Desdemona without good reason"),
-            new AdventureQuestion("You find Desdemona sleeping peacefully in bed at night. What’s your move?", "606000Stare at her lovingly and get into bed with her", "202080Suffocate her to death with a pillow.",  "106070Kill yourself."),
-            new AdventureQuestion("Barbantio accuses you of bewitching his daughter into marrying you while the entire Venentian senate is watching. How do you respond?", "250000Avoid the question", "990000Politely deny it", "506050Attack Barbantio"),
-            new AdventureQuestion( "You made it safely to Cyprus", "255050Throw a party to celebrate!","705050No time to play; Get back to work ASAP.", "505050"),
-            new AdventureQuestion( "At the party, you find Cassio drunk and fighting with Roderigo and Montano. From Iago, you learn that Cassio has injured Montano in their quarrel. How do you reprimand him?", "505080Strip Cassio of his lieutenancy.", "502050Do nothing", "505050"),
-            new AdventureQuestion( "Iago acts suspiciously, suggesting that Desdemona is having an affair with Cassio. He tells you that he saw Cassio use the handkerchief YOU gave her to wipe his beard. How will you react?", "500000Vow to kill her.", "506550Wait for more evidence before making a decision", "507010Choose to trust Desdemona over Iago"),
-            new AdventureQuestion("You see Desdemona on the bed. What do you do?", "000099Kill her.", "606030Wake her up and talk through it.",  "000020Kill yourself."),
-            new AdventureQuestion("The end of Game", "505050 ", "505050 ", "505050 ")
+            new AdventureQuestion("Barbantio accuses you of bewitching his daughter into marrying you while the entire Venentian senate is watching. How do you respond?", "305050Avoid the question", "605050Politely deny it", "202050Attack Barbantio"),
+            new AdventureQuestion( "You made it safely to Cyprus", "405060Throw a party to celebrate!","605050Get back to work, no time for playing.", "505050 "),
+            new AdventureQuestion( "At the party, you find Cassio drunk and fighting with Roderigo and Montano. From Iago, you learn that Cassio has injured Montano in their quarrel. How do you reprimand him?", "504080Strip Cassio of his lieutenancy.", "605050Do nothing", "505050"),
+            new AdventureQuestion("Desdemona repeatedly asks you to forgive Cassio and make him lieutenant again. How do you respond to her?","607020Forgive Cassio.","504060Put it off.","503070Ignore her."), //only if answer before was throw a party
+            new AdventureQuestion( "Iago acts suspiciously, suggesting that Desdemona is having an affair with Cassio. He tells you that he saw Cassio use the handkerchief YOU gave her to wipe his beard. How will you react?", "303080Vow to kill her.", "504050Wait for more evidence before making a decision", "606010Choose to trust Desdemona over Iago"),
+            new AdventureQuestion("Lodovico greets you and Desdemona with news from Venice. Cassio will replace you, and you must return back to Venice. What do you do?","103070Hit Desdemona.","404070Call Desdemona a Strumpet.","505020Choose not to be mad at Desdemona without good reason"),
+            new AdventureQuestion("You find Desdemona sleeping peacefully in bed at night. What’s your move?", "508040Stare at her lovingly and get into bed with her", "303080Suffocate her to death with a pillow.",  "105030Kill yourself."),
+            new AdventureQuestion("The end of Game", "505050 ", "505050 ", "505050Click any button to close game, Thanks for playing!")
     };
 
-    // Declare an array of "fortunes" (strings):
-    //String[] fortunes = {"You will be happy for today only. eifjiesjfoisejfiejijsieofjiosej foisejfoisejfoisjefoiseo ifjisoejf", "You find Desdemona sleeping in bed, what do you do?", "You will find happiness.", "You will find peace."};
     public int questionPlace=0;
     JFrame window;
-//    BufferedImage myPicture = ImageIO.read(new File("/Users/benagin/Library/Mobile Documents/com~apple~CloudDocs/APCS/OthelloAdventure/68512519-cyprus-wallpapers.jpg"));
-//    JLabel picLabel = new JLabel(new ImageIcon(myPicture));
+
 
     AdventureQuestion questionNow = questions[questionPlace];
 
@@ -64,7 +44,6 @@ public class Screen extends JFrame implements ActionListener
     JProgressBar Sanity= new JProgressBar(0,100);
     JProgressBar TrustInIago= new JProgressBar(0,100);
 
-    //String[] question1= {"" , "", "" , ""};
 
     private JTextArea displayQ;
     private JTextArea displayA;
@@ -82,14 +61,13 @@ public class Screen extends JFrame implements ActionListener
         displayQ.setText("Welcome to the Othello \"Choose Your Adventure\" Game! \n\n Made By: Ben Agin and  Sarah Zhang");
         displayQ.setBackground(Color.WHITE);
         displayQ.setEditable(false);
-        Color black= new Color(200,200,200);
-        displayQ.setBackground(black);
+        Color gray= new Color(200,200,200);
+        displayQ.setBackground(gray);
 
 
-        //JButton buttonA = new JButton("A");
+
         buttonA.addActionListener(this);
         buttonA.setForeground(Color.RED);
-        //buttonA.setBounds(100,400,500,20);
         displayA = new JTextArea(MAX_NUMBER_ROWS,MAX_NUMBER_COLS);
         displayA.setLineWrap(true);
         displayA.setWrapStyleWord(true);
@@ -98,7 +76,7 @@ public class Screen extends JFrame implements ActionListener
         displayA.setEditable(false);
 
 
-        //JButton buttonB = new JButton("B");
+
         buttonB.addActionListener(this);
         buttonB.setForeground(Color.RED);
 
@@ -109,12 +87,9 @@ public class Screen extends JFrame implements ActionListener
         displayB.setBackground(Color.WHITE);
         displayB.setEditable(false);
 
-        //JButton buttonC = new JButton("C");
+
         buttonC.addActionListener(this);
         buttonC.setForeground(Color.RED);
-        //buttonC.setToolTipText(questionNow.getAnswerC().substring(0,2)+" "+ questionNow.getAnswerC().substring(2,4)+" "+ questionNow.getAnswerC().substring(4,6));
-
-
         displayC = new JTextArea(MAX_NUMBER_ROWS,MAX_NUMBER_COLS);
         displayC.setLineWrap(true);
         displayC.setWrapStyleWord(true);
@@ -123,11 +98,9 @@ public class Screen extends JFrame implements ActionListener
         displayC.setEditable(false);
 
 
-
-
-
         Container c = getContentPane();
         c.setLayout(new FlowLayout());
+
         c.add(displayQ);
 
         c.add(buttonA);
@@ -140,7 +113,7 @@ public class Screen extends JFrame implements ActionListener
         c.add(displayC);
 
 
-        c.add(Respect);
+        c.add(Respect); //all bars
         c.add(Sanity);
         c.add(TrustInIago);
 
@@ -148,18 +121,22 @@ public class Screen extends JFrame implements ActionListener
         Respect.setStringPainted(true);
         Respect.setValue(80);
         Respect.setToolTipText("How much respect you have in the community");
+        Respect.setForeground(Color.BLUE);
 
         Sanity.setString("Sanity");
         Sanity.setStringPainted(true);
         Sanity.setValue(100);
         Sanity.setToolTipText("How mentally sane you are");
+        Sanity.setForeground(Color.RED);
 
-        //TrustInIago.setString("TrustInIago");
+
         TrustInIago.setStringPainted(true);
         TrustInIago.setValue(50);
         TrustInIago.setString("Trust in Iago");
         TrustInIago.setToolTipText("How much Trust you have in Iago, consider this when you make your choices");
+        TrustInIago.setForeground(Color.GREEN);
     }
+
 
     public void actionPerformed(ActionEvent e)
     {
@@ -188,47 +165,50 @@ public class Screen extends JFrame implements ActionListener
 //        catch(IOException c)
 //
 //        {System.exit(3);}
+
+// elsewhere
+
 //
+
+        if ((e.getSource()==buttonB || e.getSource()==buttonC) && questionPlace == 2) //adds another to skip drunk Cassio
+        {
+            questionPlace+=2; //2 because next 2 deal with Cassio
+
+        }
 
         questionPlace++;
 
-        if (Respect.getValue() <= 0 )
+        if (Respect.getValue() <= 0 || TrustInIago.getValue()==100 || Sanity.getValue() == 0)
         {
         questionPlace= questions.length-1;
+        //end game resonse later.
 
         }
+
 
         if (questionPlace>=questions.length)
         {
 
+            //if (questionPlace>questions.length) ;
+
+            ding.play();
+            System.exit(1);
             return;
+            //makes it so nothing happens when all questions or end game.
 
         }
 
-        //if (questionPlace ==2 && TrustInIago.getValue() <= 40)
-         //   questionPlace++;
-
-        //questionNow
         questionNow = questions[questionPlace];
         displayQ.setText(questions[questionPlace].getQuestion());
         displayB.setText(questions[questionPlace].getAnswerB().substring(6));
         displayA.setText(questions[questionPlace].getAnswerA().substring(6));
         displayC.setText(questions[questionPlace].getAnswerC().substring(6));
 
-        int buttonCInfo1= Integer.parseInt(questionNow.getAnswerC().substring(0,2)+50);
-        int buttonAInfo= questionNow.getEffects(0, questionNow.getAnswerC())-50;
-
-//        int buttonCInfo1= Integer.parseInt(questionNow.getAnswerC().substring(0,2)+50);
-//        int buttonCInfo1= Integer.parseInt(questionNow.getAnswerC().substring(0,2)+50);
 
         buttonA.setToolTipText((questionNow.getEffects(0, questionNow.getAnswerA())-50)+" "+ (questionNow.getEffects(2, questionNow.getAnswerA())-50)+" "+ (questionNow.getEffects(4, questionNow.getAnswerA())-50));
         buttonB.setToolTipText((questionNow.getEffects(0, questionNow.getAnswerB())-50)+" "+ (questionNow.getEffects(2, questionNow.getAnswerB())-50)+" "+ (questionNow.getEffects(4, questionNow.getAnswerB ())-50));
         buttonC.setToolTipText((questionNow.getEffects(0, questionNow.getAnswerC())-50)+" "+ (questionNow.getEffects(2, questionNow.getAnswerC())-50)+" "+ (questionNow.getEffects(4, questionNow.getAnswerC())-50));
 
-    //
-
-// buttonB.setToolTipText(questionNow.getAnswerB().substring(0,2)+" "+ questionNow.getAnswerB().substring(2,4)+" "+ questionNow.getAnswerB().substring(4,6));
-//        buttonA.setToolTipText(questionNow.getAnswerA().substring(0,2)+" "+ questionNow.getAnswerA().substring(2,4)+" "+ questionNow.getAnswerA().substring(4,6));
 
         if (Respect.getValue() <= 0 )
         {
@@ -236,11 +216,22 @@ public class Screen extends JFrame implements ActionListener
 
         }
 
+        if (Sanity.getValue() <= 0 )
+        {
+            displayA.setText("Your Sanity reached 0 because you obsessed over Desdemona's \"affair\" with Cassio.");
+
+        }
+
+        if (TrustInIago.getValue() >= 99 )
+        {
+            displayA.setText("Your Trust in Iago Bar has reached 100, so high that Iago abused your friendship to kill you.");
+
+        }
+
+
         ding.play();
 
 
-
-        //
     }
 
     public void adjustBar(char ans)
@@ -291,3 +282,17 @@ public class Screen extends JFrame implements ActionListener
 //        {System.exit(3);}
     }
 }
+
+//class ImagePanel extends JComponent {
+//    private Image image;
+//    public ImagePanel(Image image) {
+//        this.image = image;
+//    }
+//    @Override
+//    protected void paintComponent(Graphics g) {
+//        super.paintComponent(g);
+//        g.drawImage(image, 0, 0, this);
+//    }
+//}
+
+    // elsewhere
